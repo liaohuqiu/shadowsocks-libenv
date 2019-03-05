5308b77cdbe9e75c6b92ba083591698db9439da2
liaohuqiu <liaohuqiu@gmail.com>
2019-03-05 19:46:41 +0800

    update docker

--- manager.py
+++ manager.py
@@ -6,68 +6,61 @@ from ruamel import yaml
 
 from cpbox.tool import dockerutil
 from cpbox.tool import functocli
-from cpbox.tool import file
 from cpbox.tool import template
-from cpbox.tool import utils
 
 from cpbox.app.devops import DevOpsApp
-
-APP_NAME = 'ss'
-bypass_ipset_name = 'ss_spec_wan_bypass'
-forward_ipset_name = 'ss_spec_wan_fw'
-
-ss_image = 'docker-genesis.yizhoucp.cn/boris1993/shadowsocks-v2ray-docker:v3.2.4-1.1.0'
-# ss_image = 'docker-genesis.yizhoucp.cn/easypi/shadowsocks-libev'
-ss_container = 'ss'
-
-obfs_image = 'docker-genesis.yizhoucp.cn/liaohuqiu/simple-obfs'
-obfs_container = 'obfs'
-
-chinadns_image = 'docker-genesis.yizhoucp.cn/liaohuqiu/chinadns'
-chinadns_container = 'chinadns'
-
-ss_image='boris1993/shadowsocks-v2ray-docker:v3.2.4-1.1.0'
-ss_port=8080
-ss_container='ss'
-ss_v2ray_container='ss-v2ray'
 
 class App(DevOpsApp):
 
     def __init__(self, **kwargs):
         DevOpsApp.__init__(self, 'ss')
-        file.ensure_dir(self.app_config_dir)
-        file.ensure_dir(self.app_templates_dir)
 
         self.ss_image = 'liaohuqiu/ss'
         self.ss_v2ray_container = 'ss-v2ray'
-        self.simple_obfs_container = 'ss-obfs'
+        self.ss_obfs_container = 'ss-obfs'
 
-    def stop(self):
-        self.stop_container(self.ss_v2ray_container)
-        self.stop_container(self.simple_obfs_container)
+    def stop_ss_v2ray(self):
+        self.remove_container(self.ss_v2ray_container, force=True)
 
-    def run_ss(self):
-        # _run_ss_container "ss-server -p 8388 -m aes-128-cfb -k $password -u --acl=/etc/local.acl"
-        # _run_ss_container "ss-server -p 8388 -m aes-128-cfb -k $password -u"
-        cmd = "ss-server -s 0.0.0.0 -p 8388 -m aes-128-cfb -k $password -u --plugin v2ray-plugin --plugin-opts server"
-        self._run_ss_container(cmd)
+    def start_ss_v2ray(self):
+        cmd = "ss-server -s 0.0.0.0 -p 8388 -m aes-128-cfb -k 123466 -u --plugin v2ray-plugin --plugin-opts server --acl=/etc/local.acl"
+        ports = ['9443:8388/tcp', '9443:8388/udp']
+        self._run_container(self.ss_v2ray_container, cmd, '-it', ports=ports)
+
+    def stop_ss_obfs(self):
+        self.remove_container(self.ss_obfs_container, force=True)
+
+    def start_ss_obfs(self):
+        cmd = "ss-server -s 0.0.0.0 -p 8388 -m aes-128-cfb -k 123466 -u --plugin obfs-server --plugin-opts 'obfs=tls' --acl=/etc/local.acl"
+        ports = ['8443:8388/tcp', '8443:8388/udp']
+        self._run_container(self.ss_obfs_container, cmd, '-d', ports=ports)
+
+    def restart_ss_v2ray(self):
+        self.stop_ss_v2ray()
+        self.start_ss_v2ray()
+
+    def restart_ss_obfs(self):
+        self.stop_ss_obfs()
+        self.start_ss_obfs()
 
     def build_image(self):
         cmd = 'docker build -t %s %s/docker' % (self.ss_image, self.root_dir)
         self.shell_run(cmd)
 
-    def run_obfs(self):
-        pass
+    def cli(self):
+        container_name = 'ss-cli'
+        self.remove_container(container_name, force=True)
+        cmd = 'sh'
+        self._run_container(container_name, cmd, '-it')
 
-    def _run_container(self, image_name, container_name, cmd, run_mod='d'):
+    def _run_container(self, container_name, cmd, run_mod='d', ports=[]):
         envs = []
-        ports = []
         volumes = {
                 self.app_config_dir + '/local.acl': '/etc/local.acl'
                 }
         args = dockerutil.base_docker_args(container_name=container_name, envs=envs, ports=ports, volumes=volumes)
         cmd_data = {}
-        cmd_data['image'] = image_name
+        cmd_data['image'] = self.ss_image
         cmd_data['args'] = args
         cmd_data['cmd'] = cmd
         cmd_data['run_mod'] = run_mod
